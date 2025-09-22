f71ac87698f46d24bac1570af7270430ebc7b49f
dadam <adam.dorian37@gmail.com>
2025-09-22 11:25:00 +0200

    création des fichiers pour les dossiers de chaques salles

---
+++ app/Controllers/salle_3/Salle3Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace salle_3;
+
+use App\Controllers\BaseController;
+
+class Salle3Controller extends BaseController
+{
+
+}
---
+++ app/Models/salle_6/Salle6Model.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\salle_6;
+
+use CodeIgniter\Model;
+
+class Salle6Model extends Model
+{
+
+}
