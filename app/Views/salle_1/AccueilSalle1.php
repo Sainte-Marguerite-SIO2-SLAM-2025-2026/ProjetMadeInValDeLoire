8886ba39d8eca73351bed4db18b6c5f6a8761907
Mathys <mathysdauvergne@gmail.com>
2025-09-29 10:57:58 +0200

    Squelette salle 1

--- app/Views/salle_1/AccueilSalle1.php
+++ app/Views/salle_1/AccueilSalle1.php
@@ -1,1 +1,13 @@
-<?php
+<!doctype html>
+<html lang=fr>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Salle 1 - Ingénierie sociale</title>
+</head>
+<body>
+<?= anchor(base_url(), 'public/accueil/Accueil', "Retour au menu");  ?>
+</body>
+</html>
