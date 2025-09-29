b4bb1e97b518f6905ec6ebafd792aceadaf15445
pepito-250 <benben.pinto@gmail.com>
2025-09-29 17:02:22 +0200

    debut salle 4

--- app/Views/salle_4/AccueilSalle4.php
+++ app/Views/salle_4/AccueilSalle4.php
@@ -1,1 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salle 4</title>
+</head>
+<body>
+<h1>SALLE 4</h1>
+<nav>
+    <?= anchor(base_url().'public/', '<button>retour</button>'); ?>
+</nav>
+</body>
+</html>
