30f6e005d62c47092a5f32cc0b8bb20a57738cd7
e.menini <enzo.menini37@gmail.com>
2025-10-06 09:02:02 +0200

    vrai message de commit s'il te plaît

--- app/Views/salle_2/AccueilSalle2.php
+++ app/Views/salle_2/AccueilSalle2.php
@@ -1,1 +1,16 @@
-<?= anchor(base_url().'public/', '<button>Retour</button>'); ?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Salle 2</title>
+</head>
+<body>
+
+    <?= anchor(base_url().'public/', '<button>Retour</button>'); ?>
+</body>
+</html>
+
+
