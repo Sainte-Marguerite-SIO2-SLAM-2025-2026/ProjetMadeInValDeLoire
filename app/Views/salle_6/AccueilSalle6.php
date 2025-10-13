bfaa6339aaa7b11e137f470e57a92f094490d7ea
Clovis Jouanneau <jouanneauclovis@gmail.com>
2025-10-13 14:24:54 +0200

    Modif salle6

--- app/Views/salle_6/AccueilSalle6.php
+++ app/Views/salle_6/AccueilSalle6.php
@@ -6,9 +6,23 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Salle n°6</title>
+    <link rel="stylesheet" href="<?= base_url() ?>/styles/salle6.css">
 </head>
 <body>
+<div class="container">
     <h1>Salle n°6</h1>
-</body>
-</html>
-<?=anchor(base_url() . 'public/', '<button>Retour</button>');?>
+    <!-- Bulle de dialogue -->
+    <div class="bulle" style="top: 10%; left: 60%;">
+        <?= $intitule ?>
+    </div>
+    <!-- Mascotte avec helper img() -->
+    <?= img([
+            'src'   => 'images/mascotte.png',
+            'alt'   => 'Mascotte',
+            'class' => 'mascotte'
+    ]) ?>
+    <button><?=anchor(base_url() . '/Salle6', 'Retour');?></button>
+
+</div>
+
+
