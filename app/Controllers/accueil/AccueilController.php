b6a9588a9a077b65276ccffe79ae8d9261ef616d
Clovis Jouanneau <jouanneauclovis@gmail.com>
2025-10-13 16:36:15 +0200

    Création du header

--- app/Controllers/accueil/AccueilController.php
+++ app/Controllers/accueil/AccueilController.php
@@ -13,31 +13,36 @@ class AccueilController extends BaseController
 
     public function Salle1() : string
     {
-        return view('salle_1\AccueilSalle1').
+        return view('commun\header').
+            view('salle_1\AccueilSalle1').
             view('commun\footer');
     }
 
     public function Salle2() : string
     {
-        return view('salle_2\AccueilSalle2').
+        return view('commun\header').
+            view('salle_2\AccueilSalle2').
             view('commun\footer');
     }
 
     public function Salle3() : string
     {
-        return view('salle_3\AccueilSalle3').
+        return view('commun\header').
+            view('salle_3\AccueilSalle3').
             view('commun\footer');
     }
 
     public function Salle4() : string
     {
-        return view('salle_4\AccueilSalle4').
+        return view('commun\header').
+            view('salle_4\AccueilSalle4').
             view('commun\footer');
     }
 
     public function Salle5() : string
     {
-        return view('salle_5\AccueilSalle5').
+        return view('commun\header').
+            view('salle_5\AccueilSalle5').
             view('commun\footer');
     }
 
--- app/Controllers/salle_6/Salle6Controller.php
+++ app/Controllers/salle_6/Salle6Controller.php
@@ -9,17 +9,22 @@ class Salle6Controller extends BaseController
     public function Index() : string
     {
         $data['intitule'] = "Ouah ce train à l'aire étrange cliquez dessus pour en savoir plus";
-        return view('salle_6\AccueilSalle6',$data).
+        return view('commun\header').
+            view('salle_6\AccueilSalle6',$data).
             view('commun\footer');
     }
 
     public function Wifi():string
     {
-        return view('salle_6\Wifi');
+        return view('commun\header').
+            view('salle_6\Wifi').
+            view('commun\footer');
     }
 
     public function Vpn():string
     {
-        return view('salle_6\Vpn');
+        return view('commun\header').
+            view('salle_6\Vpn').
+            view('commun\footer');
     }
 }
