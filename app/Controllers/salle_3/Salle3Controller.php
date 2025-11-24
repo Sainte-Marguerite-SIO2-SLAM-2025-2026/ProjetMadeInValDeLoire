6dbeac68361b76013117af935bd6ca8f4713f32a
e.menini <enzo.menini37@gmail.com>
2025-11-24 17:18:51 +0100

    MAJ salle 2

--- app/Controllers/salle_3/Salle3Controller.php
+++ app/Controllers/salle_3/Salle3Controller.php
@@ -1,10 +1,26 @@
 <?php
 
-namespace salle_3;
+namespace salle_3\Salle3Controller;
 
 use App\Controllers\BaseController;
+use App\Models\salle_3\Salle3Model;
+use App\Models\salle_3\IndicesModel;
+use CodeIgniter\RESTful\ResourceController;
 
 class Salle3Controller extends BaseController
 {
+    public function index() : string
+    {
+        $idActivite = 1;
 
+        $model = new Salle3Model();
+
+        $data = $model->orderBy('RAND()')->limit(10)->find();
+
+        $modelIndices = new IndicesModel();
+        $indices = $modelIndices->getIndices($idActivite);
+
+        return view('salle_3/EnigmeSalle3', ['mails' => $data, 'idActivite' => $idActivite, 'indices' => $indices]).
+            view('commun/footer');
+    }
 }
