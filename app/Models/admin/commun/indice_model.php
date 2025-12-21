a1b43b3acf664959057cf5abf91dbf3ca9dbd62c
Clovis Jouanneau <jouanneauclovis@gmail.com>
2025-12-21 19:55:14 +0100

    correction salle3 + mauvais copier coller

--- app/Models/admin/commun/indice_model.php
+++ app/Models/admin/commun/indice_model.php
@@ -21,7 +21,7 @@ class IndiceAdminModel extends Model
     protected function getSalleRange(int $salleNumero): array
     {
         if ($salleNumero === 3) {
-            return ['min' => 1, 'max' => 399];
+            return ['min' => 1, 'max' => 99];
         }
         
         return [
